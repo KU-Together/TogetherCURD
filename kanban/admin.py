15f421310ba066222b3c807c7872670ebc657bca
GwonilJoo <gwonil3256@gmail.com>
2022-02-27 02:07:39 +0900

    Create admin.py

---
+++ kanban/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import User, Board, List, Card
+
+admin.site.register(User)
+admin.site.register(Board)
+admin.site.register(List)
+admin.site.register(Card)
