fcaad9da1ffbfcd148df931f0a0422ba3bae1d38
GwonilJoo <gwonil3256@gmail.com>
2022-02-27 02:06:19 +0900

    Update urls.py

--- TogetherCURD/urls.py
+++ TogetherCURD/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from kanban.views import UserViewset
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^user/(?P<user_id>\d+)/$', UserViewset.as_view({'get':'retrieve'})),
+    path('user', UserViewset.as_view({'post':'create'}))
 ]
